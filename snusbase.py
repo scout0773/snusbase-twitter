006b76a1cec5a54333f8ac52e2a6be2d3b33a978
4m <70970973+8pz@users.noreply.github.com>
2023-11-27 16:42:28 -0800

    Update snusbase.py

--- snusbase.py
+++ snusbase.py
@@ -1,37 +1,28 @@
-import json
 import requests
 
 snusbase_auth = 'API_KEY'
 snusbase_api = 'https://api-experimental.snusbase.com/'
 
-def send_request(url, body=None):
-    headers = {
-        'Auth': snusbase_auth,
-        'Content-Type': 'application/json',
-    }
-    method = 'POST' if body else 'GET'
-    data = json.dumps(body) if body else None
-    response = requests.request(method, snusbase_api + url, headers=headers, data=data)
-    if response.status_code != 200:
-        raise Exception('request failed', response.status_code, response.reason)
-    return response.json()
-
 def check_twitter_breach(username):
-    search_response = send_request('data/search', {
+    url = f'{snusbase_api}data/search'
+    body = {
         'terms': [username],
         'types': ['username'],
         'breach': 'TWITTER_COM_228M_SOCIAL_012023',
         'wildcard': False,
-    })
+    }
+    headers = {'Auth': snusbase_auth, 'Content-Type': 'application/json'}
+    response = requests.post(url, headers=headers, json=body)
 
-    if not search_response.get('results'):
-        return print(f"The username {username} was not found in the Twitter 2023 breach.")
+    if response.status_code != 200:
+        raise Exception('Request failed', response.status_code, response.reason)
 
-    for breach_name, breach_data in ((name, data) for name, data in search_response.get('results', {}).items() if data):
+    results = response.json().get('results', {})
+    for breach_name, breach_data in results.items():
         email = breach_data[0].get('email')
         if email:
             print(f"{username}: {email}")
-    
+
 if __name__ == '__main__':
     while True:
         check_twitter_breach(input("Enter the username to check: "))
